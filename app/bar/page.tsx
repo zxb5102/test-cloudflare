4413b02e508cbefb420c3ffe6a2f249c48508f27
zhongxb <pulangke_zhong@ikscrm.com>
2024-03-19 15:23:02 +0800

    feat: 添加cookie支持

--- app/bar/page.tsx
+++ app/bar/page.tsx
@@ -1,5 +1,17 @@
+import { cookies } from "next/headers";
+
 export default () => {
-    return <div className="mr-4">
-        bar page
+  const cookieStore = cookies();
+  const dd = cookieStore.getAll().map((item) => {
+    return {
+      name: item.name,
+      value: item.value,
+    };
+  });
+  return (
+    <div>
+      <div className="mr-4">bar page</div>
+      <div> {JSON.stringify(dd)}</div>
     </div>
-}
+  );
+};
